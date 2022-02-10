47586155d617ac5ebed759e54ee9b43938a0d7fc
Paata Kurdadze <paata2016@gmail.com>
2022-02-10 16:14:36 +0400

    Add project files.

---
+++ Enumerable/MyEnumerable.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Test_Enumerable
+{
+    public static class MyEnumerable
+    {
+        public static T MyAggregate<T>(this IEnumerable<T> source, Func<T, T, T> func)
+        {
+            if (source == null || !source.Any() || func == null) throw new ArgumentNullException();
+
+            var e = source.GetEnumerator();
+            var result = e.Current;
+
+            while (e.MoveNext())
+                result = func(result, e.Current);
+
+            return result;
+        }
+
+
+        public static IEnumerable<TResult> MySelectMany<T, TResult>(this IEnumerable<T> source,
+            Func<T, IEnumerable<TResult>> selector)
+        {
+            if (source == null || selector == null) throw new ArgumentNullException();
+
+            foreach (var item in source)
+                foreach (var result in selector(item))
+                    yield return result;
+        }
+
+
+        public static Dictionary<TKey, TSource> MyToDictionary<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            if (source == null || keySelector == null) throw new ArgumentNullException();
+
+            var dictionary = new Dictionary<TKey, TSource>();
+            foreach (var element in source)
+                dictionary.Add(keySelector(element), element);
+            return dictionary;
+        }
+
+
+        public static Dictionary<TKey, TElement> MyToDictionary<TSource, TKey, TElement>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector)
+        {
+            if (source == null || keySelector == null || elementSelector == null) throw new ArgumentNullException();
+
+            var dictionary = new Dictionary<TKey, TElement>();
+            foreach (var element in source)
+                dictionary.Add(keySelector(element), elementSelector(element));
+            return dictionary;
+        }
+
+
+        public static IEnumerable<(TFirst First, TSecond Second)> MyZip<TFirst, TSecond>(this IEnumerable<TFirst> first,
+            IEnumerable<TSecond> second)
+        {
+            if (first == null || second == null) throw new ArgumentNullException();
+
+            var eFirst = first.GetEnumerator();
+            var eSecond = second.GetEnumerator();
+
+            while (eFirst.MoveNext() && eSecond.MoveNext())
+                yield return (eFirst.Current, eSecond.Current);
+        }
+
+        public static IEnumerable<TResult> MyZip<TFirst, TSecond, TResult>(this IEnumerable<TFirst> first,
+            IEnumerable<TSecond> second, Func<TFirst, TSecond, TResult> resultSelector)
+        {
+            if (first == null || second == null || resultSelector == null) throw new ArgumentNullException();
+
+            var eFirst = first.GetEnumerator();
+            var eSecond = second.GetEnumerator();
+
+            while (eFirst.MoveNext() && eSecond.MoveNext())
+                yield return resultSelector(eFirst.Current, eSecond.Current);
+        }
+
+
+        public static IEnumerable<IGrouping<TKey, TSource>> MyGroupBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            if (source == null || keySelector == null) throw new ArgumentNullException();
+
+            foreach (var item in GetGroupedCollection(source, keySelector))
+                yield return new Grouping<TKey, TSource>(item.Key, item.Value);
+        }
+
+
+        public static IEnumerable<TResult> MyGroupBy<TSource, TKey, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            Func<TKey, IEnumerable<TSource>, TResult> resultSelector)
+        {
+            if (source == null || keySelector == null || resultSelector == null) throw new ArgumentNullException();
+
+            foreach (var item in GetGroupedCollection(source, keySelector))
+                yield return resultSelector(item.Key, item.Value);
+        }
+
+        private static Dictionary<TKey, List<TSource>> GetGroupedCollection<TSource, TKey>(IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            Dictionary<TKey, List<TSource>> dictionary = new();
+
+            foreach (var item in source)
+            {
+                var keyCurrent = keySelector(item);
+
+                if (dictionary.ContainsKey(keyCurrent)) dictionary[keyCurrent].Add(item);
+                else dictionary.Add(keyCurrent, new() { item });
+            }
+
+            return dictionary;
+        }
+
+
+        public static IEnumerable<TResult> MyOfType<TResult>(this IEnumerable source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            foreach (var item in source)
+                if (item is TResult result) yield return result; 
+        }
+        
+    }
+
+    internal class Grouping<TKey, TSource> : IGrouping<TKey, TSource>
+    {
+        private TKey _key;
+        private IEnumerable<TSource> _elements;
+
+        public TKey Key { get; }
+
+        public Grouping(TKey key, IEnumerable<TSource> elements)
+        {
+            _key = key;
+            _elements = elements;
+        }
+
+        public IEnumerator<TSource> GetEnumerator() => _elements.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+}
+
+
+
+
+
+/*
+ *  public static IEnumerable<IGrouping<TKey, TSource>> MyGroupBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            List<Grouping<TKey, TSource>> list = new();
+
+            foreach (var itemS in source)
+            {
+               
+                var keyCurrent = keySelector(itemS);
+                if(!list.Any()) list.Add(new Grouping<TKey, TSource>(keyCurrent, itemS as IEnumerable<TSource>));
+                foreach (var element in list)
+                {
+                    if (keyCurrent.Equals(element.Key))
+                    {
+                        element._elements.ToList().Add(itemS);
+                    }
+                    else
+                    {
+                        list.Add(new Grouping<TKey, TSource>(keyCurrent, itemS as IEnumerable<TSource>));
+                    }
+                }
+            }
+
+            return list;
+        }
+
+ */
---
+++ Enumerable/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Test_Enumerable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var r = new Random();
+
+            IEnumerable<int> numbers = Enumerable.Repeat(0, 4).Select(x => r.Next(20)).ToList();
+            numbers.ToList().ForEach(x => Console.WriteLine(x));
+
+            Console.WriteLine();
+
+            int sum = numbers.Aggregate((x, y) => { return x + y; });
+            int max = numbers.MyAggregate((x, y) => Math.Max(x, y));
+
+
+            
+            ////  agregacia romelic momidzebnis mxolod kenti ricxvebis maximums
+            int oddMax = numbers.MyAggregate((x, y) => y % 2 != 0 ? Math.Max(x, y) : x);
+            
+            ////SelectMany 
+            //ToDictionary 
+            //Zip da Lookup 
+            //GroupBy
+            //OfType
+            
+            
+            /////////
+            
+
+            ////SelectMany - daareserchet da daweret
+            //var list = new List<string>() { "abcd", "abcd" };
+            //var methodSyntax = list.MySelectMany(x => x);
+
+
+            //ToDictionary - daareserchet da daweret
+            //var myDictionary = numbers.MyToDictionary(x => x, x => true);
+
+
+            ////Zip da Lookup - ertad gavarchiot
+            /*      
+                  int[] numbersSequence = { 10, 20, 30, 40, 50 };
+                  string[] wordsSequence = { "Ten", "Twenty", "Thirty", "Fourty" };
+                  var resultSequence = numbersSequence.MyZip(wordsSequence, (first, second) => first + " - " + second);
+                  foreach (var item in resultSequence)
+                  {
+                      Console.WriteLine(item);
+                  }
+            */
+
+            /*
+                        ////GroupBy
+                        IList<Student> studentList = new List<Student>()
+                        {
+                            new() { StudentID = 1, StudentName = "John", Age = 18 } ,
+                            new() { StudentID = 2, StudentName = "Steve",  Age = 21 } ,
+                            new() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+                            new() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+                            new() { StudentID = 5, StudentName = "Abram" , Age = 21 }
+                        };
+
+                        //var groupedResult1 = from s in studentList group s by s.Age;
+
+                        var groupedResult2 = studentList.MyGroupBy(s => s.Age);
+            */
+
+            ////GroupBy Example2
+            /*          
+                      List<Pet> petsList =
+                              new List<Pet>{ new Pet { Name="Barley", Age=8.3 },
+                              new Pet { Name="Boots", Age=4.9 },
+                              new Pet { Name="Whiskers", Age=1.5 },
+                              new Pet { Name="Daisy", Age=4.3 } };
+
+                      var query = petsList.MyGroupBy(
+                          pet => Math.Floor(pet.Age),
+                          (age, pets) => new
+                          {
+                              Key = age,
+                              Count = pets.Count(),
+                              Min = pets.Min(pet => pet.Age),
+                              Max = pets.Max(pet => pet.Age)
+                          });
+
+                      foreach (var result in query)
+                      {
+                          Console.WriteLine("\nAge group: " + result.Key);
+                          Console.WriteLine("Number of pets in this age group: " + result.Count);
+                          Console.WriteLine("Minimum age: " + result.Min);
+                          Console.WriteLine("Maximum age: " + result.Max);
+                      }
+          */
+
+
+            //OfType
+
+            /*
+            object[] array = new object[4];
+            array[0] = new StringBuilder();
+            array[1] = "example";
+            array[2] = new int[1];
+            array[3] = "another";
+
+            // Filter the objects by their type.
+            // ... Only match strings.
+            // ... Print those strings to the screen.
+            var result = array.MyOfType<string>();
+            foreach (var element in result)
+            {
+                Console.WriteLine(element);
+            }
+            
+            */
+        }
+
+        public class Student
+        {
+            public Student()
+            {
+            }
+
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+            public int Age { get; set; }
+        }
+
+        public class Pet
+        {
+            public string Name { get; set; }
+            public double Age { get; set; }
+        }
+
+        public class Product
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public double Price { get; set; }
+        }
+    }
+}
+
+
+/*
+ * foreach (var item in groupedResult2)
+               {
+                   Console.WriteLine($"KEY: {item.Key}:");
+                   foreach (var element in item)
+                   {
+                       Console.WriteLine($"\t {element.Age}, {element.StudentName}, {element.StudentName}, {element.StudentID}");
+                   }
+               }
+ */
